d5cf1b606d675cf52c74eec43218e3702cff804b
Vasily Levit <vasily.levit@gmail.com>
2022-10-10 15:18:01 +0800

    var

--- bot.py
+++ bot.py
@@ -2,20 +2,20 @@
 from pytube import YouTube
 import telebot
 
-# bot = telebot.TeleBot('5636976148:AAHl3nSmTgOpQTXJ2gzBr1vSxmcXbvDVZo4')
+bot = telebot.TeleBot('5636976148:AAHl3nSmTgOpQTXJ2gzBr1vSxmcXbvDVZo4')
 
-# @bot.message_handler(commands = ['start'])
-# def start(message):
-#     link = f'{message.from_user.first_name}, введите ссылку на видео: '
-#     bot.send_message(message.chat.id, link)
-#     yt = YouTube(link)
-#     bot.send_message(message.chat.id, f'Длина видео: {yt.length} секунд')
-#     yt = yt.streams.get_by_itag("18")    #скачивание
-#     yt.download('Downloads')
-#     bot.send_message(message.chat.id, f'Видео скачано')
-#     bot.send_video(message.chat.id, str(yt) + '.mp4') #загрузка видео в чат
+@bot.message_handler(commands = ['start'])
+def start(message):
+    link = f'{message.from_user.first_name}, введите ссылку на видео: '
+    bot.send_message(message.chat.id, link)
+    yt = YouTube(link)
+    bot.send_message(message.chat.id, f'Длина видео: {yt.length} секунд')
+    yt = yt.streams.get_by_itag("18")    #скачивание
+    yt.download('Downloads')
+    bot.send_message(message.chat.id, f'Видео скачано')
+    bot.send_video(message.chat.id, str(yt) + '.mp4') #загрузка видео в чат
 
-# bot.polling(none_stop=True)
+bot.polling(none_stop=True)
 
 
 # from pytube import YouTube
@@ -29,16 +29,16 @@ import telebot
 
 
 
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+# from telegram import Update
+# from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+# async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+#     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
-app = ApplicationBuilder().token("YOUR TOKEN HERE").build()
-print('server start')
-app.add_handler(CommandHandler("hello", hello))
+# app = ApplicationBuilder().token("5499615048:AAFo2ioBn_aw3EKKSOM1nqzkFMUTUqYcEV0").build()
+# print('server start')
+# app.add_handler(CommandHandler("hello", hello))
 
-app.run_polling()
+# app.run_polling()
